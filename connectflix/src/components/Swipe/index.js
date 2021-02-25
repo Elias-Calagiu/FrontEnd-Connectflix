49b4980759762d1f0425d90746a2257fe720e31d
Elias Calagiu <eliasc81@gmail.com>
2021-02-25 12:01:48 -0800

    created getGenres function in swipe component and it is retrieving genres from netflix api in console

--- connectflix/src/components/Swipe/index.js
+++ connectflix/src/components/Swipe/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
 import CardBtn from "../CardBtn"
+import API from "../../utils/API"
 export default class Swipe extends Component {
+    state = {
+        search: "",
+        movies: [],
+        results: [],
+        error: ""
+      };
+    componentDidMount() {
+        API.getGenres()
+        //   .then(res => this.setState({ movies: res.data }))
+        //   .catch(err => console.log(err));
+      }
+    
+      handleInputChange = event => {
+        this.setState({ search: event.target.value });
+      };
+    
+    //   handleFormSubmit = event => {
+    //     event.preventDefault();
+    //     API.getDogsOfBreed(this.state.search)
+    //       .then(res => {
+    //         if (res.data.status === "error") {
+    //           throw new Error(res.data.message);
+    //         }
+    //         this.setState({ results: res.data.message, error: "" });
+    //       })
+    //       .catch(err => this.setState({ error: err.message }));
     render() {
         return (
             <div
                 className="card"
+                
                 style={{
-                    backgroundImage: this.props.image ? `url(${this.props.image})` : "none"
+                    backgroundImage: this.props.image ? `url(${this.props.img})` : "none"
                 }}
+                
             >
                 {!this.props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
                 <CardBtn
--- connectflix/src/utils/API.js
+++ connectflix/src/utils/API.js
@@ -1,32 +1,53 @@
 import React from "react"
 import axios from "axios";
+const URL_PREFIX = "http://localhost:8080"
 
-const options = {
-  method: 'GET',
-  url: 'https://unogsng.p.rapidapi.com/search',
-  params: {
-    start_year: '1972',
-    orderby: 'rating',
-    audiosubtitle_andor: 'and',
-    limit: '10',
-    subtitle: 'english',
-    countrylist: '78,46',
-    audio: 'english',
-    country_andorunique: 'unique',
-    offset: '0',
-    end_year: '2019'
-  },
-  headers: {
-    'x-rapidapi-key': 'cf26f75fedmsh908d182a13ab0aap13e5dejsn966016d6915d',
-    'x-rapidapi-host': 'unogsng.p.rapidapi.com'
+export default{
+  getGenres: function(){
+    const options = {
+      method: 'GET',
+      url: 'https://unogsng.p.rapidapi.com/genres',
+      headers: {
+        'x-rapidapi-key': 'cf26f75fedmsh908d182a13ab0aap13e5dejsn966016d6915d',
+        'x-rapidapi-host': 'unogsng.p.rapidapi.com'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
   }
-};
+}
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+// const options = {
+//   method: 'GET',
+//   url: 'https://unogsng.p.rapidapi.com/search',
+//   params: {
+//     start_year: '1972',
+//     orderby: 'rating',
+//     audiosubtitle_andor: 'and',
+//     limit: '10',
+//     subtitle: 'english',
+//     countrylist: '78,46',
+//     audio: 'english',
+//     country_andorunique: 'unique',
+//     offset: '0',
+//     end_year: '2019'
+//   },
+//   headers: {
+//     'x-rapidapi-key': 'cf26f75fedmsh908d182a13ab0aap13e5dejsn966016d6915d',
+//     'x-rapidapi-host': 'unogsng.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+ 
+// }).catch(function (error) {
+// 	console.error(error);
+// });
 
 // const API = {
 //   getMovies: ()=>{
