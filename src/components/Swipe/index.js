5b610fd3296eddc8149ac9903433adaaf5f96f90
Elias Calagiu <eliasc81@gmail.com>
2021-03-04 22:53:46 -0800

    users friends liked movies logging in frontend console

--- src/components/Swipe/index.js
+++ src/components/Swipe/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import CardBtn from "../CardBtn"
 import API from "../../utils/API"
-import { Card } from "antd"
 import axios from 'axios'
-import Button from '@material-ui/core/Button';
+// import {Button} from '@material-ui/core/Button';
+// import { response } from 'express'
 // import Card from "../Card"
 export default class Swipe extends Component {
     constructor(props) {
@@ -76,10 +76,11 @@ export default class Swipe extends Component {
             console.log(movieState);
             if (btnType === "pick") {
                 axios.defaults.headers.common["Authorization"] = `Bearer ${this.state.token}`;
-
-                axios.post("http://localhost:8080/api/likes", movieState).then(
-                    // only working for two users who must be friends with each other 
-                    data=>{}).catch(err=>console.log(err))
+                API.getUserFriendLikes()
+                .then(response =>{
+                    console.log(response);
+                })
+                axios.post("http://localhost:8080/api/likes", movieState).then(data=>console.log(data)).catch(err=>console.log(err))
             }
             
             this.setState({
